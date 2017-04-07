4ec6f639d2a60911c4dd4155136a7948c0914865
Mohan Liu <mohan.liu.personal@gmail.com>
2017-04-07 11:07:23 -0400

    finish the pages

--- main.rb
+++ main.rb
@@ -14,21 +14,30 @@ class Page
   field :content, :type => String
 end
 
+class Users
+  include Mongoid::Document
+  field :username,   :type => String
+  field :email,   :type => String
+  field :encrypted_password,   :type => String
+  field :salt,   :type => String
+  field :time_stamp,   :type => Date
+end
+
  get '/pages' do
    @pages = Page.all
    @title = "Simple CMS: Page List"
    slim :index
  end
 
+ get '/pages/new' do
+   @page = Page.new
+   slim :new
+ end
+
  get '/pages/:id' do
    @page = Page.find(params[:id])
    @title = @page.title
    slim :show
- end
-
- get '/pages/new' do
-   @page = Page.new
-   slim :new
  end
 
  post '/pages' do
