109b0b4a3e5423fdb8c250e8d7d7584a16b86bcd
Mohan Liu <mohan.liu.personal@gmail.com>
2017-04-07 08:56:21 -0400

    init

---
+++ main.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'mongoid'
+require 'slim'
+require 'redcarpet'
+
+configure do
+  Mongoid.load!("./mongoid.yml")
+end
+
+class Page
+  include Mongoid::Document
+
+  field :title,   :type => String
+  field :content, :type => String
+end
+
+ get '/pages' do
+   @pages = Page.all
+   @title = "Simple CMS: Page List"
+   slim :index
+ end
+
+ get '/pages/:id' do
+   @page = Page.find(params[:id])
+   @title = @page.title
+   slim :show
+ end
+
+ get '/pages/new' do
+   @page = Page.new
+   slim :new
+ end
+
+ post '/pages' do
+    page = Page.create(params[:page])
+    redirect to("/pages/#{page.id}")
+ end
